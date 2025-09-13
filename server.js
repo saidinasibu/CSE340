cb4c0a092af44d26322bde0b408dc786ac9c1173
David Nasibu <adjinasibusaidi@gmail.com>
2025-09-13 12:33:41 -0700

    updated

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ * Routes
+ *************************/
+
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
