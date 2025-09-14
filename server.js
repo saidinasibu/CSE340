69304e6e73446ec926182c952b9e7750e0ed6be0
David Nasibu <adjinasibusaidi@gmail.com>
2025-09-14 08:41:39 -0700

    updated

--- server.js
+++ server.js
@@ -38,3 +38,8 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
+})
