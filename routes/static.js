afde8304d946b3f27b19b132cc9db75dcc958a0a
David Nasibu <adjinasibusaidi@gmail.com>
2025-09-16 13:42:42 -0700

    UPDATED

--- routes/static.js
+++ routes/static.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Static Routes
-// Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
--- server.js
+++ server.js
@@ -4,18 +4,15 @@ const expressLayouts = require("express-ejs-layouts")
 
 const app = express();
 
-// Définir le moteur de vue EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Middleware layouts
 app.use(expressLayouts)
 app.set("layout", "layouts/layout")
 
-// Middleware pour fichiers statiques (CSS, images, etc.)
+
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route principale
 app.get("/", (req, res) => {
   res.render("index", { 
     title: "Accueil",
@@ -23,7 +20,6 @@ app.get("/", (req, res) => {
   });
 });
 
-// Démarrer le serveur
 const PORT = 5500;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
